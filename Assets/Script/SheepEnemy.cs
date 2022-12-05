95069b6515d6bfe12e81281041c4456718df2043
Anil Celik Maral <amaral@ucsc.edu>
2022-12-05 23:06:03 +0100

    Updated the sheep AI a little more.

--- Assets/Script/SheepEnemy.cs
+++ Assets/Script/SheepEnemy.cs
@@ -25,6 +25,24 @@ public class Sheep_Enemy : Enemy
     ActionStates CurrentActionState;
     PositionStates CurrentPositionState;
 
+    const int WALKCARDID = 1;
+    const int RUNTOPCARDID = 2;
+    const int RUNDOWNCARDID = 3;
+    const int RUNLEFTCARDID = 4;
+    const int RUNRIGHTCARDID = 5;
+    const int HEADBUTTCARDID = 6;
+    const int RUSHATTACKCARDID = 7;
+    const int PHOTOSYNTHESISCARDID = 8;
+
+    bool HasWalkCardAtHand = false;
+    bool HasRunTopCardAtHand = false;
+    bool HasRunDownCardAtHand = false;
+    bool HasRunLeftCardAtHand = false;
+    bool HasRunRightCardAtHand = false;
+    bool HasHeadbuttCardAtHand = false;
+    bool HasRushAttackCardAtHand = false;
+    bool HasPhotosynthesisCardAtHand = false;
+
     Sheep_Enemy(int ID)
     {
         this.ID = ID;
@@ -37,6 +55,46 @@ public class Sheep_Enemy : Enemy
     {
         Vector2 PlayerPosition = data.playerData.position;
         Vector2 SelfPosition = data.EnemyDataList[ID].position;
+        Vector2 DifferenceVector = PlayerPosition - SelfPosition;
+        float DifferenceInXCoordinate = Math.Abs(PlayerPosition.x - SelfPosition.x);
+        float DifferenceInYCoordinate = Math.Abs(PlayerPosition.y - SelfPosition.y);
+
+        // Check which cards we have and set the correct flags
+        foreach (var ACardAtHand in data.playerData.handCard)
+        {
+            if (ACardAtHand.ID == WALKCARDID)
+            {
+                HasWalkCardAtHand = true;
+            }
+            else if (ACardAtHand.ID == RUNTOPCARDID)
+            {
+                HasRunTopCardAtHand = true;
+            }
+            else if (ACardAtHand.ID == RUNDOWNCARDID)
+            {
+                HasRunDownCardAtHand = true;
+            }
+            else if (ACardAtHand.ID == RUNLEFTCARDID)
+            {
+                HasRunLeftCardAtHand = true;
+            }
+            else if (ACardAtHand.ID == RUNRIGHTCARDID)
+            {
+                HasRunRightCardAtHand = true;
+            }
+            else if (ACardAtHand.ID == HEADBUTTCARDID)
+            {
+                HasHeadbuttCardAtHand = true;
+            }
+            else if (ACardAtHand.ID == RUSHATTACKCARDID)
+            {
+                HasRushAttackCardAtHand = true;
+            }
+            else // if (ACardAtHand.ID == PHOTOSYNTHESISCARDID)
+            {
+                HasPhotosynthesisCardAtHand = true;
+            }
+        }
 
 
         // Are we in range?
@@ -57,24 +115,49 @@ public class Sheep_Enemy : Enemy
 
         // Are we in range?
         // If the enemy is directly , at most, 3 grid units to the right, left, above or below the enemy,
-        // then the enemy is within range.
+        // then the enemy is within range. We check if we are aligned in our x or y axis to make sure we
+        // are not at a diagonal position to the player.
         if ((PlayerPosition.x == SelfPosition.x || PlayerPosition.y == SelfPosition.y) && 
-            (Math.Abs(PlayerPosition.x - SelfPosition.x) <= 3 || Math.Abs(PlayerPosition.y - SelfPosition.y) <= 3))
+            (DifferenceInXCoordinate <= 3 || DifferenceInYCoordinate <= 3))
         {
         }
         // If no, try to move into range
         else
         {
             // Do we have a movement card?
-            if (data.playerData.handCard.Contains("ID for the walk card") || data.playerData.handCard.Contains("ID for the run card"))
+            if (HasWalkCardAtHand == true ||
+                HasRunTopCardAtHand == true ||
+                HasRunDownCardAtHand == true ||
+                HasRunLeftCardAtHand == true ||
+                HasRunRightCardAtHand == true)
             {
-                // Do we have both cards?
-                if (data.playerData.handCard.Contains("ID for the walk card") && data.playerData.handCard.Contains("ID for the run card"))
+                // Do we have both the walk and run cards?
+                if (HasWalkCardAtHand == true && (HasRunTopCardAtHand == true ||
+                                                  HasRunDownCardAtHand == true ||
+                                                  HasRunLeftCardAtHand == true ||
+                                                  HasRunRightCardAtHand == true))
                 {
-                    // If we have both cards and the distance to get into range is greater then 1 grid, then use the run card.
-                    if ((Math.Abs(PlayerPosition.x - SelfPosition.x) > 1 || Math.Abs(PlayerPosition.y - SelfPosition.y) > 1))
+                    // If we have both types of cards and the distance to get into range is greater then 1 grid, then use a run card.
+                    if (DifferenceInXCoordinate > 5 || DifferenceInYCoordinate > 5)
                     {
-                        // Play the run card!
+                        // If we are closer in the x coordinate then use a run card in that direction unless our x position is aligned with that of the player
+                        // because once we align an axis, the goal is to just get closer in the opposite axis and get in range to attack. We don't want to destroy
+                        // our allignment.
+                        if ((DifferenceInXCoordinate < DifferenceInYCoordinate) && !(PlayerPosition.x == SelfPosition.x))
+                        {
+
+                        }
+                        // If we are closer in the y coordinate then use a run card in that direction unless our y position is aligned with that of the player
+                        // because once we align an axis, the goal is to just get closer in the opposite axis and get in range to attack. We don't want to destroy
+                        // our allignment.
+                        else if ((DifferenceInXCoordinate < DifferenceInYCoordinate) && !(PlayerPosition.x == SelfPosition.x))
+                        {
+
+                        }
+                        else if (DifferenceInXCoordinate == DifferenceInYCoordinate) // CONTINUE
+                        {
+
+                        }
                     }
                     // If we have both cards and the distance to get into range is equal to 1 grid, then use the run card.
                     else if ((Math.Abs(PlayerPosition.x - SelfPosition.x) == 1 || Math.Abs(PlayerPosition.y - SelfPosition.y)== 1))
@@ -94,5 +177,15 @@ public class Sheep_Enemy : Enemy
                 // Discard a card at hand!
             }
         }
+
+        // Reset the flags
+        HasWalkCardAtHand = false;
+        HasRunTopCardAtHand = false;
+        HasRunDownCardAtHand = false;
+        HasRunLeftCardAtHand = false;
+        HasRunRightCardAtHand = false;
+        HasHeadbuttCardAtHand = false;
+        HasRushAttackCardAtHand = false;
+        HasPhotosynthesisCardAtHand = false;
     }
 }
