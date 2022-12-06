11f09ae79b6a19c1398f2af9abfef458bd202451
Mingyang Li <mingyang.china@hotmail.com>
2022-12-06 21:03:15 +0100

    adapt deck to battle data, finish movetimeline, bug fix and and test except shownotation

--- Assets/Script/UI.cs
+++ Assets/Script/UI.cs
@@ -5,53 +5,133 @@ using UnityEngine.UI;
 
 public class UI : MonoBehaviour
 {
+    //left first 
+    static GameObject card1;
+    static GameObject card2;
+    static GameObject card3;
+    static GameObject card4;
+
     static Slider health;
     static Slider energy;
     //static int RAM;
 
     static GameObject timeline;
     const int TIME_LINE_LENGTH = 10;
-    const float CARD_HEIGHT = 2;
+    const float CARD_HEIGHT = 1;
     const float CARD_WIDTH = 1;
+    const float TIME_LINE_HEIGHT = 0.5f;
+    const float TIME_LINE_SPEED = 1.0f;
 
-
-    //static List<List<Vector3>> pos;
     static Vector3[,] pos;
+    static Vector3[] handcardPos;
+    static GameObject[,] timelineObj;
 
     static Vector3 mouseWorldPos;
 
-    public static void MoveTimeLine(List<List<Card.InfoForActivate>> timeLineSlots)
-    {
-        for (int i = 0; i < timeLineSlots.Count; ++i)
+
+
+    //**************************************************************************
+    //**************************************************************************
+    // TEST CASE BEGIN
+    /*    private void Start()
         {
-            for (int j = 0; j < timeLineSlots[i].Count; ++j)
+
+
+            BattleData.playerData.maxEnergy = 20;
+            BattleData.playerData.currentEnergy = 18;
+
+            BattleData.playerData.maxHealth = 20;
+            BattleData.playerData.currentHealth = 10;
+
+
+            Card testcard1 = new Card();
+            testcard1.Name = "TestCard";
+            testcard1.ID = 1;
+
+            Card testcard2 = new Card();
+            testcard2.Name = "Circle";
+            testcard2.ID = 0;
+
+            BattleData.playerData.handCard = new List<Card> { testcard1, testcard1, testcard1, testcard1 };
+            LoadBattleBegin();
+
+            List<List<Card>> testlist = new List<List<Card>>();
+            List<Card> list1 = new List<Card> { testcard2, testcard2, testcard2 };
+            List<Card> list2 = new List<Card> { testcard2, testcard2 };
+            List<Card> list3 = new List<Card> { testcard2, };
+            List<Card> list4 = new List<Card> { testcard1, testcard1, testcard2 };
+            testlist.Add(list1);
+            testlist.Add(list2);
+            testlist.Add(list3);
+            testlist.Add(list4);
+
+            UpdateTimeLine(testlist);
+        }
+
+        public static void UpdateTimeLine(List<List<Card>> timeLineSlots)
+        {
+            List<List<Card>> timeLineSlotsTest = timeLineSlots;
+            for (int i = 0; i < timeLineSlots.Count; ++i)
             {
-                GameObject.Instantiate(Resources.Load(timeLineSlots[i][j].Name) as GameObject,
-            timeline.transform.position + pos[i, j], timeline.transform.rotation);
+                for (int j = 0; j < timeLineSlots[i].Count; ++j)
+                {
+                    if (i < 3 || (i > 2 && timeLineSlots[i][j].ID == 0))
+                    {
+                        timelineObj[j,i] = GameObject.Instantiate(Resources.Load(timeLineSlots[i][j].Name) as GameObject,
+                timeline.transform.position + pos[j, i], Quaternion.identity);
+
+                    }
+
+                }
             }
         }
-    }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                MoveTimeLine();
+            }
+        }*/
+    // TEST CASE END
+    //**************************************************************************
+    //**************************************************************************
+
     public static void UpdateHandCard()// After drawing a new card, reorgnize the hand card(align right) and move a card from deck to hand at the most left side.
     {
-        for(int i = 0; i < BattleData.playerData.handCard.Count; i++)
-        {
+        DestroyHandcard();
+        card1 = GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[0].Name) as GameObject,
+           handcardPos[0], Quaternion.identity);
 
-        }
-        //GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[0].Name) as GameObject, 
-        //    card1.transform.position, card1.transform.rotation);
+        card2 = GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[1].Name) as GameObject,
+            handcardPos[1], Quaternion.identity);
 
-        //GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[1].Name) as GameObject,
-        //    card2.transform.position, card2.transform.rotation);
+        card3 = GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[2].Name) as GameObject,
+            handcardPos[2], Quaternion.identity);
 
-        //GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[2].Name) as GameObject,
-        //    card3.transform.position, card3.transform.rotation);
-
-        //GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[3].Name) as GameObject,
-        //    card4.transform.position, card4.transform.rotation);
+        card4 = GameObject.Instantiate(Resources.Load(BattleData.playerData.handCard[3].Name) as GameObject,
+            handcardPos[3], Quaternion.identity);
     }
 
-    public static void UpdateTimeLine()
+    public static void MoveTimeLine()
     {
+        //move one timeslot
+        for (int i = 0; i < TIME_LINE_LENGTH; ++i)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                if (i == 0 && timelineObj[j, i] != null)
+                {
+                    Destroy(timelineObj[j, i].gameObject);
+                }
+                else if (timelineObj[j, i] != null)
+                {
+
+                    timelineObj[j, i].transform.position = timeline.transform.position + pos[j, i - 1];
+
+                }
+            }
+        }
 
     }
 
@@ -69,20 +149,22 @@ public class UI : MonoBehaviour
 
         //UpdateEnemyData();
 
+        InitPos();
+        InitHandcardPos();
+
+        timeline = GameObject.Find("TimeLine");
+
+
         UpdateHandCard();
 
 
-        timeline = GameObject.Find("TimeLine");
-        InitPos();
-        UpdateTimeLine();
-        
-
     }
 
-    public static IEnumerator ShowNotation(List<GameObject> notion,Card.InfoForActivate info)
+    /*public static IEnumerator ShowNotation(List<GameObject> notion, Card.InfoForActivate info)
     {
         // notion is used to show how is the range or attack damage of a card. For example move left can be arrow pointing left covering one grid.
         //this function will show the notation in the direction coresponding to the mouse and character position. For example if the mouse is at the top side of character, then the notion will placed at the top side of character.
+        
         if()
         GameObject.Instantiate(notion, new Vector2(characterpos.x, characterpos.y), Quaternion.identity);
         GameObject.Instantiate(notion, new Vector2(mouseWorldPos.x, mouseWorldPos.y), Quaternion.identity);
@@ -99,8 +181,24 @@ public class UI : MonoBehaviour
         }
         UpdateTimeLine();
         yield return null;
-    }
+    }*/
 
+    public static void UpdateTimeLine(List<List<Card.InfoForActivate>> timeLineSlots)
+    {
+        for (int i = 0; i < timeLineSlots.Count; ++i)
+        {
+            for (int j = 0; j < timeLineSlots[i].Count; ++j)
+            {
+                if (i < 3 || (i > 2 && timeLineSlots[i][j].card.ID == 0))
+                {
+                    GameObject.Instantiate(Resources.Load(timeLineSlots[i][i].card.Name) as GameObject,
+            timeline.transform.position + pos[j, i], Quaternion.identity);
+
+                }
+
+            }
+        }
+    }
     public static void ShowDuplicationWin()
     {
 
@@ -117,19 +215,56 @@ public class UI : MonoBehaviour
 
     }
 
-    public static void InitPos()
+    private static void InitPos()
     {
 
-       pos = new Vector3[10, 3];
-       //one line under timeline for enemy, one line above for player, one line for duplication
-       float offsite = CARD_WIDTH * TIME_LINE_LENGTH / 2;
-       float heightOffsite = CARD_HEIGHT;
-       for (int i = 0; i < TIME_LINE_LENGTH; ++i)
+        timelineObj = new GameObject[3, TIME_LINE_LENGTH];
+
+        pos = new Vector3[3, TIME_LINE_LENGTH];
+        //one line under timeline for enemy, one line above for player, one line for duplication
+
+        float offsite = CARD_WIDTH * TIME_LINE_LENGTH / 2;
+        float heightOffsite = CARD_HEIGHT;
+
+        for (int i = 0; i < 3; ++i)
         {
-            for (int j = 0; j < 3; ++j)
+            if (i != 0)
             {
-                pos[i,j] = new Vector3(i * CARD_WIDTH - offsite, j * (CARD_HEIGHT / 2) - heightOffsite, 0);
+                for (int j = 0; j < TIME_LINE_LENGTH; ++j)
+                {
+                    pos[i, j] = new Vector3(j * CARD_WIDTH - offsite, i * CARD_HEIGHT - heightOffsite + TIME_LINE_HEIGHT, 0);
+                }
             }
+            else
+            {
+                for (int j = 0; j < TIME_LINE_LENGTH; ++j)
+                {
+                    pos[i, j] = new Vector3(j * CARD_WIDTH - offsite, i * CARD_HEIGHT - heightOffsite, 0);
+                }
+            }
+
         }
+    }
+
+    private static void InitHandcardPos()
+    {
+        card1 = new GameObject();
+        card2 = new GameObject();
+        card3 = new GameObject();
+        card4 = new GameObject();
+
+        handcardPos = new Vector3[4];
+        handcardPos[0] = new Vector3(1, 0.7f, 100);
+        handcardPos[1] = new Vector3(1 - 2 * CARD_WIDTH, 0.7f, 100);
+        handcardPos[2] = new Vector3(1 - 4 * CARD_WIDTH, 0.7f, 100);
+        handcardPos[3] = new Vector3(1 - 6 * CARD_WIDTH, 0.7f, 100);
+    }
+
+    public static void DestroyHandcard()
+    {
+        Destroy(card1.gameObject);
+        Destroy(card2.gameObject);
+        Destroy(card3.gameObject);
+        Destroy(card4.gameObject);
     }
 }
