64613ece69ea47aec67cf74614ff19f91333fee6
wasmann <38429018+wasmann@users.noreply.github.com>
2022-12-12 04:02:58 +0100

    cameraMovement and gridbutton.cs

---
+++ Assets/Script/CameraMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float Threshold = 10;
+    public float speed = 3;
+    public float[] border=new float[4];
+    void Update()
+    {
+        if (Input.mousePosition.x>=Screen.width-Threshold && this.transform.position.x <= border[0])
+        {    
+            transform.position += Vector3.right*Time.deltaTime * speed;
+        }
+        if(Input.mousePosition.x <= Threshold && this.transform.position.x >= border[1])
+        {
+            transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+        if (Input.mousePosition.y >= Screen.height - Threshold && this.transform.position.y <= border[2])
+        {
+            transform.position += Vector3.up * Time.deltaTime * speed;
+        }
+        if (Input.mousePosition.y <= Threshold && this.transform.position.y >= border[3])
+        {
+            transform.position += Vector3.down * Time.deltaTime * speed;
+        }
+    }
+}
---
+++ Assets/Script/TileMapButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class TileMapButton : MonoBehaviour
+{
+    public Tilemap tilemap;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y,-10)) ;
+            Vector3Int CellPos = tilemap.WorldToCell(pos);
+            Debug.Log(CellPos);
+        }
+    }
+}
